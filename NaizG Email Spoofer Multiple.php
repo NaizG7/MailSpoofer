7cfa85807c87f63440d349904d6db166aa75361a
Mateo. A <70346750+NaizG7@users.noreply.github.com>
2020-09-06 09:03:18 -0300

    Add files via upload

---
+++ NaizG Email Spoofer Multiple.php
@@ -0,0 +1,32 @@
+    <?php
+
+	//HTML Email Spoofer By NaizG 7
+	
+$Victims = array(
+  "victim @ email.com",
+  "victim @ email.com",
+  "victim @ email.com",
+  "victim @ email.com",
+  "victim @ email.com",
+  "victim @ email.com",
+);
+
+foreach($Victims as $contact) {
+
+$to      =  $contact;
+    
+    $subject = "Subject";
+    $message = file_get_contents("index.html");
+    
+    $from = "From Name From@Email.com";
+    
+    $headers = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    $headers .= "From:" . $from;
+    
+    mail($to,$subject,$message,$headers); }
+	
+	echo '<p style="text-align: center;"> <font size="4"> <b>EMAIL SENT</b> </font> <br> <b> <font size="1" face="comic sans ms, sans-serif" color="#444444">HTML Email Spoofer by Naizg7</font> </b> </p>';
+
+
+?>
